302e09dfa9fa4af2b7dea3ec63ea32b7336b2a58
Yildirim-AliD <yldrmalid@gmail.com>
2025-05-05 05:35:37 +0300

    feat: set up main screen structure and navigation with WordService
    - Initialized WordService in main and loaded all words
    - Set up MainPage with bottom navigation bar
    - Connected two base screens: WordList and AddWord placeholder
    - Verified database read using debugPrint

---
+++ vocabulary_memo_flutter/lib/screens/word_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WordList extends StatefulWidget {
+  const WordList({super.key});
+
+  @override
+  State<WordList> createState() => _WordListState();
+}
+
+class _WordListState extends State<WordList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
